142d8885d8abdae97c8162310e1d4ef81584bd36
joethemovie <joethemovie@gmail.com>
2016-08-04 08:44:42 -0400

    Update master.js

--- twitchapp/js/master.js
+++ twitchapp/js/master.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
 var streamers = ["manvsgame", "a_seagull", "esl_overwatch", "brunofin", "comster404", "seriousgaming","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "empyreanmauler", "zakivory"];
 
 // THIS WAS A SEARCH TEST. IMPLEMENTING IN THE FUTURE
-var streamerInput  = document.getElementById("commentarea");
-  $('#search').click(function(){
+// var streamerInput  = document.getElementById("commentarea");
+  // $('#search').click(function(){
 
-            var searchTerm = $('#searchTerm').val();
-            streamers.push(searchTerm);
-            console.log(streamers);
-            return streamers;
-    });
+           // var searchTerm = $('#searchTerm').val();
+           // streamers.push(searchTerm);
+           // console.log(streamers);
+          //  return streamers;
+  //  });
 
   for (var i = 0; i < streamers.length; i++) {
     $.getJSON('https://api.twitch.tv/kraken/streams/'+ streamers[i] +'?callback=?', function(data) {
