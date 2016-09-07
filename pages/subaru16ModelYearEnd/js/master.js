99ae494ba0671119344c64ad0c7ca8b920af53d5
Joe <joethemovie@gmail.com>
2016-09-07 11:23:34 -0400

    test2

--- pages/subaru16ModelYearEnd/js/master.js
+++ pages/subaru16ModelYearEnd/js/master.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   var url = "http://joethemovie.com/pages/subaru16ModelYearEnd/js/modelYear2016.json";
     $.getJSON(url, function(data) {
-    var car = data.car;
-      $('#testUl').prepend("<li>" + car + "</li>");
+    var car = data.vehicles.car;
+    var stock = data.vehicles.stock;
+    var price = data.vehicles.stock;
+      $('#testUl').prepend("<li>" + car + "</li><li>" + stock + "</li><li>" + price + "</li>");
     });
 });
