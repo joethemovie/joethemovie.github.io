032080752613ba92d0ac6cbbf242b72b6fc4a4c8
Joe <joethemovie@gmail.com>
2016-09-08 15:36:03 -0400

    Mr Do Yo Lead

--- pages/subaru16ModelYearEnd/js/master.js
+++ pages/subaru16ModelYearEnd/js/master.js
@@ -3,16 +3,18 @@ $(document).ready(function(){
   var stock;
   var price;
   var outbacks = [];
+    $.getJSON('http://joethemovie.com/pages/subaru16ModelYearEnd/js/modelYear2016.json', function(data) {
+    outbacks = data.outbacks.ob0;
+    var count = Object.keys(outbacks).length;
+ console.log(count);
 
-      $.getJSON('http://joethemovie.com/pages/subaru16ModelYearEnd/js/modelYear2016.json', function(data) {
-      outbacks = data.vehicles.outbacks;
-      outbacks = outbacks.length;
-      console.log(outbacks);
-      car = data.vehicles.outbacks.car;
-      stock = data.vehicles.outbacks.stock;
-      price = data.vehicles.outbacks.price;
-      console.log(car);
-        $('#testUl').append("<li>" + car + "</li><li>" + stock + "</li><li>" + price + "</li>");
-
-    }
+for (var i = 0; i <= count; i++) {
+  var vehicle = ob + i;
+  car = data.outbacks..car;
+  stock = data.outbacks.ob1.stock;
+  price = data.outbacks.ob1.price;
+  console.log(car);
+}
+      $('#testUl').append("<li>" + car + "</li><li>" + stock + "</li><li>" + price + "</li>");
+    });
 });
